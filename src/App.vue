1646eac1068941a8c31a676e2f03125bb9176fb1
Lv Jingxin <lv510987@163.com>
2024-04-17 15:08:54 +0800

    feat: 初始化项目

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-04-17 14:06:09
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-04-17 14:38:50
+ * @LastEditTime: 2024-04-17 15:04:43
  * @FilePath: /large-screen/App.vue
  * @Description: App
 -->
 <template>
   <div>nihao</div>
+  <repo />
 </template>
-<script setup></script>
+<script setup>
+import repo from '@/App.vue';
+</script>
 
 <style scoped></style>
