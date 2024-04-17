2ed47b2e090e9c3549558263e6f8f40ef5316b11
Lv Jingxin <lv510987@163.com>
2024-04-17 15:09:45 +0800

    prev: 去除代码

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,13 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-04-17 14:06:09
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-04-17 15:04:43
+ * @LastEditTime: 2024-04-17 15:09:14
  * @FilePath: /large-screen/App.vue
  * @Description: App
 -->
 <template>
   <div>nihao</div>
-  <repo />
 </template>
-<script setup>
-import repo from '@/App.vue';
-</script>
+<script setup></script>
 
 <style scoped></style>
