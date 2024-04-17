665a82597bda9cfa6a417773594841f5392fa2f1
Lv Jingxin <lv510987@163.com>
2024-04-17 15:40:39 +0800

    feat: 引入图片

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,12 +2,12 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-04-17 15:25:53
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-04-17 15:27:20
+ * @LastEditTime: 2024-04-17 15:36:58
  * @FilePath: /large-screen/src/views/home.vue
  * @Description: 
 -->
 <template>
-  <div class="">111</div>
+  <div class=""></div>
 </template>
 <script setup></script>
 <style lang="less" scoped></style>
