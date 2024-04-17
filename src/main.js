2f49d0016da98ac5ced51fe7ce58936a10468826
Lv Jingxin <lv510987@163.com>
2024-04-17 17:07:53 +0800

    feat: 适配插件完成头部

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-04-17 14:06:09
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-04-17 15:22:41
+ * @LastEditTime: 2024-04-17 16:48:55
  * @FilePath: /large-screen/src/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,5 +14,4 @@ console.log('VITE_BASEURL', import.meta.env.VITE_APP_NAME);
 
 const app = createApp(App);
 app.use(router); //注册路由
-
 app.mount('#app');
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,12 +2,58 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-04-17 15:25:53
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-04-17 15:36:58
+ * @LastEditTime: 2024-04-17 17:03:43
  * @FilePath: /large-screen/src/views/home.vue
  * @Description: 
 -->
 <template>
-  <div class=""></div>
+  <v-scale-screen class="screenBac" width="1920" height="1080">
+    <div class="home">
+      <!-- 头部 -->
+      <div class="top">
+        <div class="back">返回</div>
+        <div class="back">4月16日 &nbsp; 11:24</div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  </v-scale-screen>
 </template>
-<script setup></script>
-<style lang="less" scoped></style>
+<script setup>
+import VScaleScreen from 'v-scale-screen';
+</script>
+<style lang="less" scoped>
+.screenBac {
+  background: linear-gradient(
+    269.26deg,
+    rgba(5, 21, 34, 1) 0.64%,
+    rgba(12, 59, 98, 1) 43.98%,
+    rgba(12, 59, 98, 1) 55.83%,
+    rgba(7, 22, 36, 1) 99.37%
+  ) !important;
+}
+.home {
+  height: 100vh;
+  width: 100%;
+  // padding-top: 30px;
+  .top {
+    height: 112px;
+    background: url('@/assets/top.svg');
+    // 剧中
+    background-position: center center;
+    // 大小伸缩
+    background-size: cover;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 56px 0;
+    .back {
+      text-align: center;
+      background: linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(91, 224, 243, 1) 100%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      font-size: 25px;
+      margin-top: 35px;
+    }
+  }
+}
+</style>
